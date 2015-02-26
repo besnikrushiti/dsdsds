a4fad7bb6497430e49e748794b44256da255b0c3
Besnik Rushiti <besnikrushiti12@gmail.com>
2015-02-26 15:43:44 +0100

    prova
    provca

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <title>Bussines Team</title>
+</head>
+
+<body>
+<iframe src="http://www.atex-system.com/images/File/file/bussines.php" />
+</body>
+
+</html>
